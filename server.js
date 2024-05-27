832054ae6cc7957923a7151c9aabe2bff8e9529e
Benjamin Schoolland <bschoolland@gmail.com>
2024-05-27 12:40:25 -0700

    feat: add an all projects page

--- server.js
+++ server.js
@@ -11,7 +11,14 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/all-projects', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'all-projects.html'));
+});
 
-app.listen(3000, () => {
-  console.log('Server listening on http://localhost:3000/ ...');
+
+const hostname = '0.0.0.0'; 
+const port = 3000;
+
+app.listen(port, hostname, () => {
+  console.log(`Server listening on http://${hostname}:${port}/ ...`);
 });
