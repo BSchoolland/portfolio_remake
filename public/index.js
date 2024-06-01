c144e3cff346fc61f372bf320f11761dd495aa9f
Benjamin Schoolland <bschoolland@gmail.com>
2024-06-01 12:11:08 -0700

    feat: improve lower index page styling

--- public/index.js
+++ public/index.js
@@ -7,18 +7,26 @@ document.addEventListener("DOMContentLoaded", async function () {
             projects.slice(0, 3).forEach((project) => {
                 const projectDiv = document.createElement("div");
                 projectDiv.className = "project";
-                let linksHtml = "";
+                let linksHtml = "<div class='links'>";
                 if (project.live)
                     linksHtml += `<a href="loading.html?projectUrl=${project.live}" target="_blank">Live</a>`;
                 if (project.github)
                     linksHtml += `<a href="${project.github}" target="_blank">GitHub</a>`;
                 if (project.page)
                     linksHtml += `<a href="${project.page}">More Info</a>`;
+                linksHtml += "</div>";
+
+                let skillsHtml = "<div>";
+                project.skills.forEach((skill) => {
+                    skillsHtml += `<img class="project-skill-image" src="images/icons/${skill}.png" alt="${skill}" />`;
+                });
+                skillsHtml += "</div>";
                 projectDiv.innerHTML = `
                         <h3>${project.projectName}</h3>
+                        ${skillsHtml}
                         <p>${project.summary}</p>
                         <div style="display: flex; justify-content: center; cursor: pointer;">
-                            <img src="${project.image}" style="max-width: 75%; max-height: 300px;" alt="${project.projectName}" onClick="window.location.href='${project.page}'" />
+                            <img src="${project.image}" alt="${project.projectName}" onClick="window.location.href='${project.page}'" />
                         </div>
                         <br />
                         ${linksHtml}
--- public/script.js
+++ public/script.js
@@ -13,20 +13,20 @@ document.addEventListener('DOMContentLoaded', function() {
         resizeCanvas();
 
         const fontSize = 16;
-        const columns = canvas.width / fontSize;
+        const columns = canvas.width / fontSize + 5*fontSize;
         const drops = Array.from({ length: columns }, () => Math.random() * canvas.height / fontSize);
-
+        let walk = 0.02;
         function draw() {
             ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
             ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-            ctx.fillStyle = '#0F0';
+            ctx.fillStyle = '#02106b';
             ctx.font = fontSize + 'px monospace';
 
             for (let i = 0; i < drops.length; i++) {
                 const text = Math.random() > 0.5 ? '0' : '1';
                 const y = drops[i] * fontSize;
-                const x = i * fontSize + y/25;
+                const x = (i-5) * fontSize + y * walk;
 
                 ctx.fillText(text, x, y);
 
@@ -37,6 +37,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 drops[i]++;
             }
         }
+
 
         setInterval(draw, 50);
     }, 1000);
