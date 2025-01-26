de5fb9c1fca63af66836a0795288db03f836ea36
Benjamin Schoolland <bschoolland@gmail.com>
2025-01-26 08:57:51 -0800

    feat: add a ping function for BAM events

--- server.js
+++ server.js
@@ -21,13 +21,13 @@ const hostname = '0.0.0.0';
 const port = process.env.PORT || 3000; // Use environment variable for port or default to 3000
 
 // Function to ping Heroku app
-function pingHerokuApp() {
+function pingApp(url, message, start, end) {
   const currentDate = new Date();
   const currentHour = currentDate.getHours();
   // Only ping between 8 AM and 11 PM (8-23 inclusive)
-  if (currentHour >= 8 && currentHour < 23) {
-    console.log('Pinging Heroku App to keep awake...');
-    https.get('https://www.communityali.org', (res) => {
+  if (currentHour >= start && currentHour < end) {
+    console.log(message);
+    https.get(url, (res) => {
       console.log(`STATUS: ${res.statusCode}`);
     }).on('error', (e) => {
       console.error(`Got error: ${e.message}`);
@@ -35,9 +35,14 @@ function pingHerokuApp() {
   }
 }
 
-pingHerokuApp()
-// Schedule to ping every 5 minutes
-setInterval(pingHerokuApp, 5 * 60 * 1000);
+// Initial pings
+pingApp('https://www.communityali.org', "Pinging Heroku App to keep awake...", 8, 23);
+// Schedule to ping communityali.org every 5 minutes
+setInterval(() => pingApp('https://www.communityali.org', "Pinging Heroku App to keep awake...", 8, 23), 5 * 60 * 1000);
+
+pingApp('https://beanythingmuseum.org/', "Pinging BAM to update events cache...", 1, 23);
+// ping BAM every 61 minutes
+setInterval(() => pingApp('https://beanythingmuseum.org/', "Pinging BAM to update events cache...", 1, 23), 61 * 60 * 1000);
 
 app.listen(port, hostname, () => {
   console.log(`Server listening on http://${hostname}:${port}/ ...`);
